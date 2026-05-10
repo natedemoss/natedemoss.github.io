15f3c8384ab8f641c83894b8be552596bc42052e
natedemoss <ndemoss28@gmail.com>
2026-05-10 13:06:07 -0400

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://natedemoss.github.io',
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
